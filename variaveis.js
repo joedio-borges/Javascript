a88b6512e7b8ae9ac9693f6493bad6932d9ca107
Aluno IFC <aluno@lab32-pc01.sombrio.ifc.edu.br>
2024-12-06 10:56:55 -0300

    atualizacao

--- variaveis.js
+++ variaveis.js
@@ -1,31 +1,28 @@
-var nome = "Joédio";
+let nome = "Diego";
 console.log(nome);
 
 console.log("---- var ----");
 if (true) {
-    console.log("Acessível dentro do bloco: " + nome);
-    var exemploVar = "Visível fora do bloco!";
+    let nome = "diegooo";
+    console.log("Acessivel dentro do bloco!" + nome);
 }
-console.log(exemploVar); // Funciona, mas pode causar problemas.
+console.log("Acessivel fora do bloco!" + nome); // Funciona, mas pode causar problemas.
 
 console.log("---- let ----");
-let sobrenome = "Borges";
 if (true) {
-    console.log(sobrenome); //  funciona aqui.
     let exemploLet = "Visível apenas dentro do bloco!";
     console.log(exemploLet); // Funciona aqui.
 }
-//console.log(exemploLet); // Erro: exemploLet não está definido.
-console.log("Acessível globalmente: " + sobrenome);
+// console.log(exemploLet); // Erro: exemploLet não está definido.
 
 console.log("---- const ----");
 const PI = 3.14; // Valor que não muda
 console.log(PI); // 3.14
-// PI = 3.15; // Erro: Não é permitido alterar uma const
+//PI = 3.15; // Erro: Não é permitido alterar uma const
 
 function escopoLocal() {
     let local = "Escopo de função";
     console.log(local);
 }
 escopoLocal();
-// console.log(local); // Erro: 'local' é inacessível fora da função
+//console.log(local); // Erro: 'local' é inacessível fora da função MTS DIAS
